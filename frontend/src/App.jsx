82bbe981803309d05b1262f4bebbe951b316b2ca
Mayank Rana <mayank24341@iiitd.ac.in>
2026-05-02 21:37:31 +0530

    frontend init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import ChessBoard from "./components/ChessBoard";
+
+export default function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Shinsou</h1>
+      <ChessBoard />
+    </div>
+  );
+}
