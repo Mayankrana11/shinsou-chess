1951c076fc924e49bded6b7c652d1a8194c8d6b7
Mayank Rana <mayank24341@iiitd.ac.in>
2026-05-08 18:25:55 +0530

    updated main call and readme

---
+++ chess-engine/README.txt
@@ -0,0 +1,3 @@
+gcc main.c board/board.c -o shinsou
+
+./shinsou
--- chess-engine/engine-c/main.c
+++ chess-engine/engine-c/main.c
@@ -1,4 +1,7 @@
+#include <stdio.h>
+
 #include "board/board.h"
+#include "movegen/movegen.h"
 
 int main() {
 
@@ -7,6 +10,25 @@ int main() {
     initBoard(&pos);
 
     printBoard(&pos);
+
+    Move moves[MAX_MOVES];
+
+    int count = generateMoves(&pos, moves);
+
+    printf("Generated Moves: %d\n\n", count);
+
+    for(int i=0;i<count;i++) {
+
+        printf(
+            "(%d,%d) -> (%d,%d)\n",
+
+            moves[i].fromRow,
+            moves[i].fromCol,
+
+            moves[i].toRow,
+            moves[i].toCol
+        );
+    }
 
     return 0;
 }
